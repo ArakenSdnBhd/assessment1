b550e920dc23994712da2bc0f6c5b2b4c3b0f78f
AmirulAiman <aiman_amirul@ymail.com>
2017-11-11 14:41:06 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::group(['prefix' => 'admin'],function(){
+    
+    route::get('insert', function(){
+        return view('insert');
+    });
+});
