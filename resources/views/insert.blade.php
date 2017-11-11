584a53705be01beeba7175f5e8df8cbdc21978d5
rosnasihah <rosnasihah@gmail.com>
2017-11-11 14:53:45 +0800

    assessment

---
+++ resources/views/insert.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Assesment | Admin page</title>
+        <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-3"><h3>Admin page</h3></div>
+                <div class="col-sm-3"></div>
+            </div>
+            <div class="">
+                <table class="table-bordered">
+                    
+                </table>    
+            </div>
+            <div>
+            
+            </div>
+        </div>
+    </body>
+    <script src="{{ asset('js/app.js') }}"></script>
+</html>
